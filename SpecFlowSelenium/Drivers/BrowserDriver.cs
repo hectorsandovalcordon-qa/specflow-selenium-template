aed18eabd56d3e6f6a1a64c980443013559dc5e9
Héctor Sandoval Cordón <hectorsandovalcordon@gmail.com>
2025-04-25 11:32:47 +0200

    Update BrowserDriver.cs

--- SpecFlowSelenium/Drivers/BrowserDriver.cs
+++ SpecFlowSelenium/Drivers/BrowserDriver.cs
@@ -7,7 +7,18 @@ namespace SpecFlowProject.Drivers
     {
         public IWebDriver CreateDriver()
         {
-            return new ChromeDriver();
+            // Generar un directorio único temporal para cada sesión
+            var userDataDir = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+
+            // Configuración de opciones de Chrome
+            var options = new ChromeOptions();
+            options.AddArguments($"--user-data-dir={userDataDir}");  // Especifica un directorio único para cada sesión
+            options.AddArguments("--headless"); // Si quieres que se ejecute en modo headless (sin interfaz gráfica)
+
+            // Crear el ChromeDriver con las opciones configuradas
+            var driver = new ChromeDriver(options);
+
+            return driver;
         }
     }
 }
